36fd5dde3a000839b5f6312359090cb6c307d689
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-03 23:28:28 +0530

    added keystroke event

--- main.py
+++ main.py
@@ -10,13 +10,33 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player(x,y):
+    screen.blit(playerImg, (x, y))
+
 # Game Loop
 running = True
 while running:
+    
+    # RGB value
+    screen.fill((0,0,0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    # RGB value
-    screen.fill((255,0,0))
+        
+        if event.type == pygame.KEYDOWN:
+            print("A Key is pressed")
+            if event.key == pygame.K_LEFT:
+                print("Left arrow is pressed")
+            if event.key == pygame.K_RIGHT:
+                print("Right arrow is pressed")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Keystroke has been released")
+
+    player(playerX, playerY)
     pygame.display.update()
