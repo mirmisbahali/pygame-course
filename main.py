a343e94f089c60e71692e1ed852780bc8243abfa
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-03 23:03:09 +0530

    Created game loop

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
