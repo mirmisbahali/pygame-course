b649ab39cf9781f6bcbc2a1a93b565cbfe574b91
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-05 17:21:11 +0530

    created boundaries

--- main.py
+++ main.py
@@ -29,15 +29,17 @@ while running:
             running = False
         
         if event.type == pygame.KEYDOWN:
-            print("A Key is pressed")
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.1
+                playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
-
+                playerX_change = 0.3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                print("Keystroke has been released")
+                playerX_change = 0
     playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
     player(playerX, playerY)
     pygame.display.update()
